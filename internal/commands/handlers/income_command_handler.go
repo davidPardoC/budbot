333898fea41b59c96db0537e2c60fe4958df0956
davidPardoC <pardodavid10@gmail.com>
2024-08-22 21:17:46 -0500

    feat(handlers): Add income command handler

--- internal/commands/handlers/income_command_handler.go
+++ internal/commands/handlers/income_command_handler.go
@@ -1,13 +1,21 @@
 package handlers
 
 import (
+	"strconv"
+
+	"github.com/davidPardoC/budbot/internal/telegram/builders"
+	"github.com/davidPardoC/budbot/internal/telegram/constants/messages"
 	"github.com/davidPardoC/budbot/internal/telegram/services"
 	"github.com/davidPardoC/budbot/internal/users/usecases"
+
+	transactionModels "github.com/davidPardoC/budbot/internal/transactions/models"
 )
 
 type IncomeCommandHandler struct {
 	telegramService services.ITelegramService
 	userUseCases    usecases.IUserUseCases
+	amount          float64
+	description     string
 }
 
 func NewIncomeCommandHandler(telegramService services.ITelegramService, userUseCases usecases.IUserUseCases) *IncomeCommandHandler {
@@ -18,8 +26,44 @@ func NewIncomeCommandHandler(telegramService services.ITelegramService, userUseC
 }
 
 func (h *IncomeCommandHandler) HandleCommand(chatID int64, args []string) {
+
+	telegramMessageBuilder := builders.NewTelegramMessageBuilder(chatID)
+
+	if !h.ValidateArgs(args) {
+		payload := telegramMessageBuilder.SetText(messages.IncomeCommandInvalidArgsText).Build()
+		h.telegramService.SendMessage(payload)
+		return
+	}
+	_, err := h.userUseCases.RegisterTransaction(h.amount, h.description, transactionModels.Income, chatID)
+
+	if err != nil {
+		payload := telegramMessageBuilder.SetText(messages.ErrorRegisteringIncomeText).Build()
+		h.telegramService.SendMessage(payload)
+	}
+
+	payload := builders.NewTelegramMessageBuilder(chatID).SetText(messages.SuccessfullyRegisteredIncomeText).Build()
+	h.telegramService.SendMessage(payload)
 }
 
 func (h *IncomeCommandHandler) ValidateArgs(args []string) bool {
+
+	if len(args) < 2 {
+		return false
+	}
+
+	amount := args[0]
+
+	if args[1] != "" {
+		h.description = args[1]
+	}
+
+	parsedAmount, err := strconv.ParseFloat(amount, 64)
+
+	if err != nil {
+		return false
+	}
+
+	h.amount = parsedAmount
+
 	return true
 }
