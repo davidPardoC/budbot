0edecebdc9fc0d2c375bd1bb10e297c1a575ae96
davidPardoC <pardodavid10@gmail.com>
2024-08-22 21:26:48 -0500

    fix(handlers): fix incomemessages

---
+++ internal/telegram/constants/messages/income_messages.go
@@ -0,0 +1,15 @@
+package messages
+
+const (
+	IncomeCommandInvalidArgsText = `
+	Invalid arguments, please use the following format: /ri <amount> <description>.
+	`
+
+	SuccessfullyRegisteredIncomeText = `
+	Income registered successfully! 💸
+	`
+
+	ErrorRegisteringIncomeText = `
+	There was an error registering the income. Please try again later.
+	`
+)
