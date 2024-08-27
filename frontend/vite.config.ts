f8c5e88633e6f90d0d1cb5d8ecdde5e790d1a3a9
David Pardo <43886292+davidPardoC@users.noreply.github.com>
2024-08-27 00:31:50 -0500

    feat(dash): add stats dash (#18)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,10 @@
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
-import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
