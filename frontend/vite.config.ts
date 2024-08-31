11a4b4b690333abce811835a0c05c95e7da74908
davidPardoC <pardodavid10@gmail.com>
2024-08-30 23:28:28 -0500

    Remove api from workbox

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,15 +5,13 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: "autoUpdate",
-      workbox: {
-        globPatterns: ["**/*.{js,css,ico,png,svg}"],
-      },
-    }),
-  ],
+  plugins: [react(), VitePWA({
+    registerType: 'autoUpdate',
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+      navigateFallbackDenylist: [/^\/api/, /^\/auth/],
+    },
+  })],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
